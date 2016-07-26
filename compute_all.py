51bf8c0e7f2ed12e3243a2bcae6114e121f28d32
Sebastián <srvanrell@gmail.com>
2016-07-26 01:00:09 -0300

    Compute all use gspread

--- compute_all.py
+++ compute_all.py
@@ -16,29 +16,29 @@ __author__ = 'sebastian'
 # and saves the result into the same xlsx
 ##########################################
 
-tournaments_xlsx = cfg["io"]["data_folder"] + cfg["io"]["tournaments_filename"]
-rankings_xlsx = cfg["io"]["data_folder"] + cfg["io"]["rankings_filename"]
-log_xlsx = cfg["io"]["data_folder"] + cfg["io"]["log_filename"]
+spreadsheet_id = cfg["io"]["tournaments_spreadsheet_id"]
+ranking_spreadsheet_id = cfg["io"]["ranking_spreadsheet_id"]
+log_spreadsheet_id = cfg["io"]["log_spreadsheet_id"]
 
 # Listing tournament sheetnames by increasing date
-tournament_sheetnames = utils.get_sheetnames_by_date(tournaments_xlsx, cfg["sheetname"]["tournaments_key"])
+tournament_sheetnames = utils.get_sheetnames_by_date_gs(spreadsheet_id, cfg["sheetname"]["tournaments_key"])
 
 # Loading players info list
 players = models.PlayersList()
-players.load_list(utils.load_sheet_workbook(tournaments_xlsx, cfg["sheetname"]["players"]))
+players.load_list(utils.load_sheet_gs(spreadsheet_id, cfg["sheetname"]["players"]))
 
 # Loading initial ranking
-initial_ranking = utils.load_ranking_sheet(tournaments_xlsx, cfg["sheetname"]["initial_ranking"])
+initial_ranking = utils.load_ranking_sheet_gs(spreadsheet_id, cfg["sheetname"]["initial_ranking"])
 
 for tid, tournament_sheetname in enumerate(tournament_sheetnames):
-
-    tournament = utils.load_tournament_xlsx(tournaments_xlsx, tournament_sheetname)
+    # Loading tournament info
+    tournament = utils.load_tournament_gs(spreadsheet_id, tournament_sheetname)
 
     old_ranking = models.Ranking("pre_" + tournament.name, tournament.date, tournament.location, tid - 1)
 
     # Load previous ranking if exists
     if tid-1 >= 0:
-        old_ranking = utils.load_ranking_sheet(rankings_xlsx, tournament_sheetnames[tid - 1].replace(
+        old_ranking = utils.load_ranking_sheet_gs(ranking_spreadsheet_id, tournament_sheetnames[tid - 1].replace(
             cfg["sheetname"]["tournaments_key"], cfg["sheetname"]["rankings_key"]))
 
     # Load initial rankings for new players
@@ -71,19 +71,18 @@ for tid, tournament_sheetname in enumerate(tournament_sheetnames):
     assigned_participation_points = new_ranking.add_participation_points(pid_participation_list)
 
     # Saving new ranking
-    utils.save_ranking_sheet(rankings_xlsx, tournament_sheetname.replace(cfg["sheetname"]["tournaments_key"],
-                                                                         cfg["sheetname"]["rankings_key"]),
-                             new_ranking, players, True)
+    utils.save_ranking_sheet_gs(ranking_spreadsheet_id, tournament_sheetname.replace(
+        cfg["sheetname"]["tournaments_key"], cfg["sheetname"]["rankings_key"]), new_ranking, players)
 
     # Saving points assigned in each match
     points_log_to_save = [[players[winner_pid].name, players[loser_pid].name, winner_points, loser_points]
                           for winner_pid, loser_pid, winner_points, loser_points in assigned_points_per_match]
 
-    utils.save_sheet_workbook(log_xlsx,
-                              tournament_sheetname.replace(cfg["sheetname"]["tournaments_key"],
-                                                           cfg["sheetname"]["rating_details_key"]),
-                              [cfg["labels"][key] for key in ["Winner", "Loser", "Winner Points", "Loser Points"]],
-                              points_log_to_save, True)
+    utils.save_sheet_gs(log_spreadsheet_id,
+                        tournament_sheetname.replace(cfg["sheetname"]["tournaments_key"],
+                                                     cfg["sheetname"]["rating_details_key"]),
+                        [cfg["labels"][key] for key in ["Winner", "Loser", "Winner Points", "Loser Points"]],
+                        points_log_to_save)
 
     # Saving points assigned per best round reached and for participation
     points_log_to_save = [[players[pid].name, points, best_round, category] for pid, points, best_round, category
@@ -91,8 +90,8 @@ for tid, tournament_sheetname in enumerate(tournament_sheetnames):
     participation_points_log_to_save = [[players[pid].name, points, cfg["labels"]["Participation Points"], ""]
                                         for pid, points in assigned_participation_points]
 
-    utils.save_sheet_workbook(log_xlsx,
-                              tournament_sheetname.replace(cfg["sheetname"]["tournaments_key"],
-                                                           cfg["sheetname"]["bonus_details_key"]),
-                              [cfg["labels"][key] for key in ["Player", "Bonus Points", "Best Round", "Category"]],
-                              points_log_to_save + participation_points_log_to_save, True)
+    utils.save_sheet_gs(log_spreadsheet_id,
+                        tournament_sheetname.replace(cfg["sheetname"]["tournaments_key"],
+                                                     cfg["sheetname"]["bonus_details_key"]),
+                        [cfg["labels"][key] for key in ["Player", "Bonus Points", "Best Round", "Category"]],
+                        points_log_to_save + participation_points_log_to_save)
